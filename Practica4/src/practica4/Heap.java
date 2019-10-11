de9c4128718517117a03111216f5a7d6feaee49d
JacoboMartinezGomez <jmgomez2@esei.uvigo.es>
2019-10-11 14:49:17 +0200

    Actividad 4 resuelto primer ejercicio, falta terminar metodo suprimir

--- Practica4/src/practica4/Heap.java
+++ Practica4/src/practica4/Heap.java
@@ -9,15 +9,14 @@ package practica4;
  *
  * @author Jacobo
  */
-public class Heap <E extends Comparable<E>> {
+public class Heap<E extends Comparable<E>> {
 
     private static final int CAPACIDAD = 25;
     private E[] vector;	// el vector del montículo
-    private int numElem;	// número de elementos del montículo.
+    private int numElem;// número de elementos del montículo.
 
     public Heap(E superior) {
         numElem = 0;
-        //vector = (E[]) new Object[CAPACIDAD+1]; //esta solución no pasa el test. imposible convertir un Object en genérico
         vector = (E[]) new Comparable[CAPACIDAD + 1]; //Array de objetos comparables, en concreto E, para que coincida con el tipo de los árboles
         vector[0] = superior;
     }
@@ -28,35 +27,98 @@ public class Heap <E extends Comparable<E>> {
 
     public E recuperarMax() throws HeapVacioExcepcion {
         if (esVacio()) {
-            throw new HeapVacioExcepcion("recuperarMax: Heap vacío");
+            throw new HeapVacioExcepcion("Heap vacío");
         }
         return vector[1];
     }
 
-    public E suprimirMax() throws HeapVacioExcepcion { //En un  árbol siempre se suprime la raíz
-        if (esVacio()) {
-            throw new HeapVacioExcepcion("suprimirMax: Heap vacío");
-        }
-        E e = vector[1];
-        vector[1] = vector[numElem]; //Lleva el último elemento a la primera posición 
-        vector[numElem--] = null; //Sería igual que vector[numElem]=null; numElem--; 
-        hundir(1);
-        return e;
-    }
-
-    public void insertar(E e) throws IllegalArgumentException {
-        if (numElem == vector.length - 1) {
-            duplicarVector();
-        }
+    public void insertar(E e) {
         int hueco = ++numElem;
-        while (vector[hueco / 2].compareTo(e) < 0) { //Resta el elemento de la posicion hueco/2 con e y no para hasta que la resta sea positiva                                                        
+        while (vector[hueco / 2].compareTo(e) < 0) {
             vector[hueco] = vector[hueco / 2];
             hueco /= 2;
         }
         vector[hueco] = e;
     }
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+    public E suprimir() throws HeapVacioExcepcion {
+        //primer paso devolver la raiz y eliminarla
+        //segundo paso  recolocar hueco con el hundir
+        if (esVacio()) {
+            throw new HeapVacioExcepcion("Heap vacío");
+        } else {
+            E e = vector[1];
+            vector[1] = vector[numElem];//recolocar la raiz
+            vector[numElem] = null; //hijo eliminado a null
+            hundir(1);//Recolocar posicion de la raiz
+            return e;
+        }
+
+    }
+
+    //metodo de recolocar: hundir
+    private void hundir(int hueco) {
+        //mirar si tiene hijos == no, termina, si es si, comprobar si tiene 1 o 2
+        //si tiene 1 hijo--> hijo>hueco = intercambiar
+        //hijo<= hueco termina
+        //si tiene 2 hijos, hijoizq>hijoderecha; iz>hueco si: intercambia, no: termina
+        //hijoder>hijoizq; der>hueco si: intercambia, no: termina
+        int hijo = hueco * 2;
+        E temporal = vector[hueco];
+        boolean fin = false;
+
+        while (hijo <= numElem && !fin) {
+            if (hijo + 1 <= numElem) {//Preguntar si hay 2 hijos
+                if (vector[hijo].compareTo(vector[hijo + 1]) > 0) {//primer hijo mayor que el segundo
+                    if (vector[hijo].compareTo(temporal) > 0) {//primer objeto mayor que el segundo, menor que 0 menor primer objeto que el segundo
+                        vector[hueco] = vector[hijo];//intercambiar  
+                        hueco = hijo;
+                        hijo = hueco * 2;
+                    } else {
+                        fin = true;
+                    }
+
+                } else {
+                    if (vector[hijo].compareTo(vector[hijo + 1]) <= 0) {//segundo hijo mayor que el primero
+                        if (vector[hijo + 1].compareTo(temporal) > 0) {//primer objeto mayor que el segundo, menor que 0 menor primer objeto que el segundo
+                            vector[hueco] = vector[hijo + 1];//intercambiar  
+                            hueco = hijo + 1;
+                            hijo = hueco * 2;
+                        } else {
+                            fin = true;
+                        }
+                    }
+                }
+
+            } else {
+                if (vector[hijo].compareTo(temporal) > 0) {//primer objeto mayor que el segundo, menor que 0 menor primer objeto que el segundo
+                    vector[hueco] = vector[hijo];//intercambiar  
+                    hueco = hijo;
+                    hijo = hueco * 2;
+                } else {
+                    fin = true;
+                }
+            }
+        }//Fin del while
     }
     
+    
+    
+    //Heapsort
+    //1ºmeto los elementos en el heap, introducir()
+    //2ºarreglarheap
+    
+    //1ºy2ºpaso, se puede implementar solo con el insertar pero es menos eficiente
+    
+    //3ºsuprimir()elementos heap
+    
+    //Ejercicio2
+    
+    public void introducir(E e){
+        
+    }
+    public void arreglarHeap(){
+    
+    }
+
 }
