e1a9836c073cc34e037133b42fb03c7c47216995
JacoboMartinezGomez <jmgomez2@esei.uvigo.es>
2019-10-17 11:16:42 +0200

    cambio de nombre en ficheros y comienzo de ejercicios 1 y 2

--- Practica5/src/practica/pkg5/Main.java
+++ Practica5/src/practica/pkg5/Main.java
@@ -4,18 +4,24 @@
  * and open the template in the editor.
  */
 package practica.pkg5;
+import arbolGeneral.*;
 
 /**
  *
  * @author Jacobo
  */
-public class Practica5 {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
+        
+        
+        
     }
     
 }
